851175362c4244cdd345a2c3c13b5540067651ef
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-07-02 17:54:33 +0530

    Time: 22 ms (59.08%), Space: 42.8 MB (72.64%) - LeetHub

---
+++ 3sum/3sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int len = nums.length;
+        for(int i=0;i<len;i++) {
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int rem = -nums[i];
+            int end = len - 1;
+            int start = i+1;
+            while(start<end) {
+                if(start>i+1 && nums[start] == nums[start-1]) {
+                    start++;
+                    continue;
+                }
+                int currsum = nums[start] + nums[end];
+                if(currsum == rem) {
+                    result.add(Arrays.asList(nums[i],nums[start],nums[end]));
+                    start++;
+                } else if (currsum < rem) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        return result;
+    }
+    
+    
+}
