d38c18f4bcf7124fbb355d0e96b1484b5efd0944
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-07-02 18:09:10 +0530

    Time: 20 ms (70.83%), Space: 42.8 MB (79.90%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -3,18 +3,17 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         int len = nums.length;
-        int currsum, start,end,rem;
         for(int i=0;i<len;i++) {
             if(i>0 && nums[i] == nums[i-1]) continue;
-            rem = -nums[i];
-            end = len - 1;
-            start = i+1;
+            int rem = -nums[i];
+            int end = len - 1;
+            int start = i+1;
             while(start<end) {
                 if(start>i+1 && nums[start] == nums[start-1]) {
                     start++;
                     continue;
                 }
-                currsum = nums[start] + nums[end];
+                int currsum = nums[start] + nums[end];
                 if(currsum == rem) {
                     result.add(List.of(nums[i],nums[start],nums[end]));
                     start++;
