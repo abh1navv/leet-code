320632efa20bb7ab72a41a79e46bf5c0924dbdae
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-07-04 17:45:10 +0530

    Time: 5 ms (60.44%), Space: 39.4 MB (52.13%) - LeetHub

--- roman-to-integer/roman-to-integer.java
+++ roman-to-integer/roman-to-integer.java
@@ -9,20 +9,18 @@ class Solution {
         romToInt.put('D',500);
         romToInt.put('M',1000);
         
-        int num = 0;
+        int num = 0, i=0;
         int len = s.length();
-        for(int i=0;i<len;i++) {
+        for(i=0;i<len-1;i++) {
             char ch = s.charAt(i);
-            if(ch == 'I' && i<len-1 && (s.charAt(i+1) == 'V' || s.charAt(i+1) == 'X')) {
-                num+=romToInt.get(s.charAt(i+1)) - romToInt.get(ch); i++;
-            } else if(ch == 'X' && i<len-1 && (s.charAt(i+1) == 'L' || s.charAt(i+1) == 'C')) {
-                num+=romToInt.get(s.charAt(i+1)) - romToInt.get(ch);       i++;         
-            } else if(ch == 'C' && i<len-1 && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M')) {
-                num+=romToInt.get(s.charAt(i+1)) - romToInt.get(ch);      i++;          
-            } else {
+            char ch2 = s.charAt(i+1);
+            if(romToInt.get(ch) >= romToInt.get(ch2)) {
                 num+=romToInt.get(ch);
-            }
+            } else {
+                num-=romToInt.get(ch);
+            }                
         }
+        num+=romToInt.get(s.charAt(i));
         return num;
     }
 }
