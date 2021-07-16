a5655b0c3e266ace42ca2eab3acdfda5dbd654a4
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-07-16 11:36:27 +0530

    Time: 1 ms (87.56%), Space: 39.6 MB (62.31%) - LeetHub

--- jump-game-ii/jump-game-ii.java
+++ jump-game-ii/jump-game-ii.java
@@ -1,32 +1,29 @@
 class Solution {
     public int jump(int[] nums) {
-        int len = nums.length;
+        int len = nums.length-1;
         int jumps = 0;
-        for(int i=0;i<len-1;) {
-            int rem = len-i-1;
-
-            if(nums[i]>=rem) {
+        for(int i=0;i<len;) {
+            int curr = nums[i];
+            if(curr+i>=len) {
                 jumps++;
                 break;
             }
             int max = nums[i+1] + i+1;
             int maxIn = i+1;
-            for(int j=1;j<=nums[i];j++) {
+            for(int j=1;j<=curr;j++) {
 
                 int index = i+j;
-              //System.out.println("Num:"+nums[index]+" : "+(len-index-1));
-
-                if(index>=len || nums[index] >= len-index-1) {
+                int dist = nums[index] + index;
+                if(index>len || dist >= len) {
                     maxIn = index;
                     break;
-                } else if (nums[index] + index >= max) {
-                    max = nums[index] + index;
+                } else if (dist >= max) {
+                    max = dist;
                     maxIn = index;
                 }
             }
             jumps++;
             i = maxIn;
-            //System.out.println(i);
         }
         
         return jumps;
