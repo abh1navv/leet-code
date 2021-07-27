42de98e8338196ab232c1414afd8c8de71f13375
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-07-27 18:48:44 +0530

    Time: 2 ms (79.74%), Space: 39.5 MB (40.21%) - LeetHub

---
+++ valid-sudoku/valid-sudoku.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        List<Set<Character>> row=new ArrayList<>();
+        List<Set<Character>> col=new ArrayList<>();
+        List<Set<Character>> block=new ArrayList<>();
+        
+        for(int i=0;i<9;i++) {
+            row.add(new HashSet<>());
+            col.add(new HashSet<>());
+            block.add(new HashSet<>());
+        }
+        
+        for(int i=0;i<9;i++) {
+            for(int j=0;j<9;j++) {
+                char value = board[i][j];
+                if(value=='.') {
+                    continue;
+                }
+                
+                if(row.get(i).contains(value)) {
+                
+                    return false;
+                }
+                else row.get(i).add(value);
+                
+                if(col.get(j).contains(value)) {
+                   
+                    return false;
+                }
+                else col.get(j).add(value);
+                
+                int blockNumber = getBlockNumber(i,j);
+                if(block.get(blockNumber).contains(value)) {
+                   
+                    return false;
+                }
+                else block.get(blockNumber).add(value);
+            }
+        }
+        
+        return true;
+    }
+    
+    public static int getBlockNumber(int i, int j) {
+        return i/3 * 3 + j/3;
+    }
+}
