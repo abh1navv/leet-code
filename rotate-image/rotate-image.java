c34cd0c5a48a66a40c98d7d1d66da761d5a594f7
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-07-28 10:15:52 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (23.94%) - LeetHub

--- rotate-image/rotate-image.java
+++ rotate-image/rotate-image.java
@@ -4,16 +4,20 @@ class Solution {
         int curr = 1;
         for(int i=0;i<len;i++) {
             for(int j=0;i+j<len-1;j++) {
+                int newCol = len-i-1;
+                int newRow = len-j-1;
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[len-j-1][len-i-1];
-                matrix[len-j-1][len-i-1] = temp;
+                matrix[i][j] = matrix[newRow][newCol];
+                matrix[newRow][newCol] = temp;
             }
         }
         for(int i=0;i<len/2;i++) {
             for(int j=0;j<len;j++) {
+                int newCol = j;
+                int newRow = len-i-1;
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[len-i-1][j];
-                matrix[len-i-1][j] = temp;
+                matrix[i][j] = matrix[newRow][newCol];
+                matrix[newRow][newCol] = temp;
             }
         }
     }
