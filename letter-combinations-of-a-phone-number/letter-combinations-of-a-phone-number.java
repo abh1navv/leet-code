f612da6eb50d57aa866a778fab12e235203d67d0
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-07-29 14:00:35 +0530

    Time: 1 ms (72.80%), Space: 37.5 MB (92.20%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,63 +1,9 @@
 class Solution {
-    Map<Character,String> charMap = new HashMap<>();
-    {   
-        charMap.put('2',"abc");
-        charMap.put('3',"def");
-        charMap.put('4',"ghi");
-        charMap.put('5',"jkl");
-        charMap.put('6',"mno");
-        charMap.put('7',"pqrs");
-        charMap.put('8',"tuv");
-        charMap.put('9',"wxyz");
-    }
+    
+    String[] charMap = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
     public List<String> letterCombinations(String digits) {
-        int len = digits.length();
-        
-        if(len==0) return new ArrayList<>();
-        
         List<String> alphaStrings = getStrings(digits);
-            //getAlphaStrings(digits.toCharArray(),0, "");
-//         for(int i=0;i<len;i++) {
-//             char ch = digits.charAt(i);
-//             List<String> newList = new ArrayList<>();
-//             char[] chars = charMap.get(ch).toCharArray();
-//             if(alphaStrings.isEmpty()) {
-//                 for(char c: chars) {
-//                     alphaStrings.add(String.valueOf(c));
-//                 }
-//             } else {
-//                 Iterator<String> iter = alphaStrings.iterator();
-                
-//                 while(iter.hasNext()) {
-//                     String curr = iter.next();
-//                     for(char c: chars) {
-//                         newList.add(curr + c);
-//                     }
-//                 }
-//                 //alphaStrings.clear();
-//                 alphaStrings = newList;
-//             }
-//         }
-        return alphaStrings;
-    }
-    
-    public List<String> getAlphaStrings(char[] chars, int index, String start) {
-        List<String> alphaStrings = new ArrayList<>(); 
-
-        if(index == chars.length) {
-            alphaStrings.add(start);
-            return alphaStrings;
-        }
-        char c = chars[index];
-        
-        char[] newChars = charMap.get(c).toCharArray();
-        //System.out.println(newChars);
-        for(char ch: newChars) {
-            List<String> tempList = getAlphaStrings(chars, index+1, start+ch);
-          //  System.out.println(tempList);
-            alphaStrings.addAll(tempList);
-        }
         return alphaStrings;
     }
     
@@ -66,7 +12,7 @@ class Solution {
         int len = digits.length();
         if(len <= 0) return strings;
         if(len == 1) {
-            char[] chars = charMap.get(digits.charAt(0)).toCharArray();
+            char[] chars = charMap[digits.charAt(0) - '0'].toCharArray();
             for(char ch:chars) {
                 strings.add(String.valueOf(ch));
             }
