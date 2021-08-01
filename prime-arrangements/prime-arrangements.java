5f96d337da62de959aecc2256a80907279e9d23e
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-08-01 11:35:53 +0530

    Time: 0 ms (100.00%), Space: 36 MB (41.51%) - LeetHub

---
+++ prime-arrangements/prime-arrangements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numPrimeArrangements(int n) {
+        int count = n>=2?1:0;
+        int negCount = 1;
+        long perms = 1;
+        for(int i=3;i<=n;i++) {
+            if(isPrime(i)) {
+                count++;
+                perms=(perms*count)%(1000000007);
+            } else {
+                negCount++;
+                perms=(perms*negCount)%(1000000007);
+            }
+        }
+        
+        return (int)(perms%1000000007);
+        
+    }
+    
+    public boolean isPrime(int n) {
+        
+        for(int i=2;i<=n/2;i++) {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
