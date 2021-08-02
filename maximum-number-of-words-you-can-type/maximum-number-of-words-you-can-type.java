049bad4241cc4c73ce1d6bcdf546a33fb8eda969
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-08-02 15:35:48 +0530

    Time: 2 ms (85.63%), Space: 38.8 MB (79.91%) - LeetHub

--- maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -2,18 +2,22 @@ class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
         String[] words = text.split(" ");
         int len = words.length;
-                boolean[] notTypeable = new boolean[len];
 
         int blen = brokenLetters.length();
+        
         for(int i=0;i<len;i++) {
-           for(char ch: brokenLetters.toCharArray()) {
-               if(notTypeable[i] == false && words[i].contains(String.valueOf(ch))) notTypeable[i] = true;
-           }
+            boolean flag = false;
+            for(char ch: brokenLetters.toCharArray()) {
+               if(len<=i) break;
+               if(words[i].indexOf(ch) != -1) {
+                   words[i] = words[--len]; 
+                   flag = true;
+               }
+            }
+            if(len<=i) break;
+            if(flag) i--;
         }
         
-        for(boolean b:notTypeable) {
-            if(b) len--;
-        }
         return len;
     }
 }
