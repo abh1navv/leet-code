92238e08f5de8fca1339312f3533b4a98c4e533a
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-09-25 12:12:25 +0530

    Time: 116 ms (87.79%), Space: 41.1 MB (91.86%) - LeetHub

---
+++ queries-on-number-of-points-inside-a-circle/queries-on-number-of-points-inside-a-circle.js
@@ -0,0 +1,30 @@
+
+/**
+ * @param {number[][]} points
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+const countPoints = function(points, queries) {
+    let result = []
+
+    queries.forEach(query => {
+        const xCord = query[0]
+        const yCord = query[1]
+        const radius = Math.pow(query[2], 2)
+        let currRes = 0
+        points.forEach(point => {
+            const xPoint = point[0]
+            const yPoint = point[1]
+            
+            const distance = Math.pow(xPoint - xCord, 2) + Math.pow(yPoint - yCord, 2)
+            if(distance <= radius) {
+                currRes++
+            }
+        })
+        
+        result.push(currRes)
+    })
+    
+    return result
+}
+
