ba26558f61f9caab287e8fcf84a4c80030c1f683
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-09-27 12:53:26 +0530

    Time: 116 ms (100.00%), Space: 60.8 MB (100.00%) - LeetHub

--- minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.js
+++ minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.js
@@ -4,31 +4,25 @@
  * @param {boolean[]} hasApple
  * @return {number}
  */
-var minTime = function(n, edges, hasApple) {
-    let incomingEdge = []
+const minTime = function(n, edges, hasApple) {
+    const incomingEdge = []
+    const set = new Set()
+
     for(let i=0; i<n-1; i++) {
         incomingEdge[edges[i][1]] = edges[i][0]
-    }
-    let toVisit = []
-    let set = new Set()
-    let steps = 0
-    for(let i=1; i< n; i++) {
-        if(hasApple[i]) 
-            set.add(i)
-    }
-    
-    //while(set.size >= 3) {
-      //  console.log(set)
-        for(let item of set) {
-            console.log(item)
-            if(item == 0) continue
-            else {
-                set.add(incomingEdge[item])
-                //set.delete(item)
-                steps+=2
-            }
+        if(hasApple[i+1]) {
+           set.add(i+1) 
         }
-    //}
+            
+    }
+    let steps = 0
+    
+    for(let item of set) {
+        if(item !== 0) {
+            set.add(incomingEdge[item])
+            steps+=2
+        }
+    }
     
     return steps
-};
+}
