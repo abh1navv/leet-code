2fbbc09f33bff3efa911a268103c66e67cf844fa
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-09-28 10:02:22 +0530

    Time: 136 ms (69.30%), Space: 49.3 MB (79.44%) - LeetHub

--- group-anagrams/group-anagrams.js
+++ group-anagrams/group-anagrams.js
@@ -2,29 +2,21 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+const groupAnagrams = function(strs) {
     const sortedStrs = {}
     const answer = []
+    let index = 0
+
     for(let item in strs) {
         const sortedItem = sortedString(strs[item])
         if(sortedStrs.hasOwnProperty(sortedItem)) {
-            sortedStrs[sortedItem].push(item)
+            answer[sortedStrs[sortedItem]].push(strs[item])
         } else {
-            sortedStrs[sortedItem] = [item]
+            sortedStrs[sortedItem] = index++
+            answer.push([strs[item]])
         }
     }
-    let index = 0
-    for(let prop in sortedStrs) {
-        const values = sortedStrs[prop]
-        const len = values.length
-        answer.push([strs[values[0]]])
-        for(let i=1; i<len; i++) {
-            const value = values[i]
-            answer[index].push(strs[value])
-        }
-        index++
-    }
-    //console.log(answer)
+    
     return answer
 }
 
