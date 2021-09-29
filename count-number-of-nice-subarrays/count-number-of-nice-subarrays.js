a3c56913cab16d6f47c884c5effc3c31e796b99c
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-09-29 18:53:17 +0530

    Time: 100 ms (64.20%), Space: 52.1 MB (38.27%) - LeetHub

--- count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
+++ count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
@@ -6,17 +6,18 @@
 const numberOfSubarrays = function(nums, k) {
     let count = 0
     const curr = [-1]
-    let first = 0
+    let first = 1
     for(let i= 0; i< nums.length; i++) {
         const item = nums[i]
         if(item % 2 === 1) {
             curr.push(i)
         } 
-        if(curr.length - first > k+1 ) { 
+        let len = curr.length
+        if( len > k+first ) { 
             first++
         }
-        if(curr.length - first === k+1 ) { 
-            count = count + (curr.length > first+1 ? curr[first+1] : i) - curr[first]
+        if( len === k + first ) { 
+            count = count + (len > first? curr[first] : i) - curr[first-1]
         }
     } 
     
