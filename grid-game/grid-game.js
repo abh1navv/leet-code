2a3eccfa8803a6b932008d85e9ab6bba20c3cde9
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-09-30 21:54:51 +0530

    Time: 96 ms (98.39%), Space: 51.1 MB (83.87%) - LeetHub

--- grid-game/grid-game.js
+++ grid-game/grid-game.js
@@ -6,9 +6,11 @@ var gridGame = function(grid) {
     let ts=0, ls = 0;
     let res = Number.POSITIVE_INFINITY;
     const len = grid[0].length
+    
     for(let j=0; j<len; j++) {
         ts += grid[0][j]
     }
+    
     for(let j=0; j<len; j++) {
         ts -= grid[0][j]
         res = Math.min(res, Math.max(ls, ts))
