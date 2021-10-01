627acdc89d3b2a37a2487a9461a868b8e365cd64
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-10-01 10:28:09 +0530

    Time: 116 ms (9.66%), Space: 38.3 MB (81.81%) - LeetHub

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -2,16 +2,13 @@
  * @param {number} numRows
  * @return {number[][]}
  */
-var generate = function(numRows) {
+const generate = function(numRows) {
     const res = [[1]]
     for(let i=0; i<numRows-1; i++) {
         res[i+1] = []
         for(let j=0; j<i+2; j++) {
-            const left = j>0 ? res[i][j-1] : 0 
-            const right = j<res[i].length ? res[i][j] : 0 
-            res[i+1].push(left+right)
+            res[i+1].push((res[i][j-1] ?? 0) + (res[i][j] ?? 0))
         }
-    }  
-    
+    }     
     return res
-};
+}
