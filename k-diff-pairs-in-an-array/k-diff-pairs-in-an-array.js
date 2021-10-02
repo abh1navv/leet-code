b4e53e93b5a02b32e82b5fa250c7149794b12ecf
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-10-02 11:46:50 +0530

    Time: 118 ms (38.75%), Space: 43 MB (60.63%) - LeetHub

---
+++ k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findPairs = function(nums, k) {
+    
+    let count = 0
+    if(k === 0) {
+        return zeroCase(nums)
+    } else {
+        const map = {}
+        for(let i=0; i< nums.length; i++) {
+            let negativeEntry = map[nums[i] - k]
+            let positiveEntry = map[nums[i] + k]
+            if(negativeEntry !== undefined && (map[nums[i]] === undefined)) {
+                count++
+            } 
+            if(positiveEntry !== undefined && (map[nums[i]] === undefined)) {
+                count++
+            }
+            map[nums[i]] = i
+        }
+    }
+    return count
+}
+    
+const zeroCase = function(nums) {
+    const map = {}
+    let count = 0
+    for(let i=0; i< nums.length; i++) { 
+        if(map[nums[i]] === undefined) {
+            map[nums[i]] = 1
+        } else if(map[nums[i]] === 1) {
+            count++
+            map[nums[i]] = 2
+        }
+    }
+    return count
+}
