a539d9a0ae15b39897781b24dd2096cb7d1b0049
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-10-12 17:16:20 +0530

    Time: 80 ms (55.35%), Space: 41 MB (37.43%) - LeetHub

--- count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
@@ -5,7 +5,7 @@
 const countNegatives = function(grid) {
     const n = grid.length
     const m = grid[0].length
-    let i=n-1, j=0, count = 0
+    let i = n-1, j = 0, count = 0
     while(i>=0 && j<m) {
         if(grid[i][j] < 0) {
             count += m - j
