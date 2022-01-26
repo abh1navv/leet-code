a92412e5b9d9be9de3c56826bb263dee43038deb
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-01-26 13:38:34 +0530

    Time: 0 ms (100.00%), Space: 39 MB (51.47%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,8 +1,7 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int maxLeft = height[0], maxRight = height[n-1];
-        int left=0, right =n-1;
+        int maxLeft = height[0], maxRight = height[height.length-1];
+        int left=0, right = height.length-1;
         int total = 0;
         
         while(left < right){
