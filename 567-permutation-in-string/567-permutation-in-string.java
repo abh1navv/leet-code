8df227612f16a835d8ca6165a6059f8625bc639c
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-02-05 20:34:17 +0530

    Time: 25 ms (36.67%), Space: 43.7 MB (16.76%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -4,34 +4,67 @@ class Solution {
     if (len1 > len2) return false;
     
     Map<Character, Integer> map = new HashMap<>();
+    int size = 0;
     for (int i = 0; i < len1; i++) {
         char ch = s1.charAt(i);
         if(map.containsKey(ch)) map.put(ch, map.get(ch)+1);
-        else map.put(ch,1);
+        else {
+            map.put(ch,1);
+            size++;
+        }
     }
     
     for (int i = 0; i < len2; i++) {
         char ch = s2.charAt(i);
-        if(map.containsKey(ch)) map.put(ch, map.get(ch)-1);
+        Integer x = map.get(ch);
+
+        if(x!=null) {
+            if(x==1) {
+                size--;
+                map.put(ch, null);
+            } else if(x==0) {
+                size++;
+                map.put(ch, x-1);
+            }
+            else map.put(ch, x-1);
+        }
         else {
+            size++;
             map.put(ch,-1);
         }
         
         if(i - len1 >= 0) {
             ch = s2.charAt(i-len1);
-            if(map.containsKey(ch)) map.put(ch, map.get(ch)+1);
-            else map.put(ch,1);
+            x = map.get(ch);
+            if(x!=null) {
+                if(x==-1) {
+                    size--;
+                    map.put(ch, null);
+                } else if(x==0) {
+                    size++;
+                    map.put(ch, x+1);
+                } else {
+                    map.put(ch, x+1);
+                }
+            }
+            else {
+                size++;
+                map.put(ch,1);
+            }
         }
         
-        if (allZero(map)) return true;
+        if (size==0){//} && isNull(map)) {
+            //System.out.println(map + " "+ i);
+            return true;
+        }
     }
     
     return false;
 }
 
-    private boolean allZero(Map<Character, Integer> map) {
-        for(int i: map.values()) {
-            if(i!=0) return false;
+    private boolean isNull(Map<Character, Integer> map) {
+        for(Integer i: map.values()) {
+            if(i!=null && i!=0) return false;
         }
         return true;
     }
