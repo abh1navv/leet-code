78b1e00820402d7e788be1c66791cde990bc8c13
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-02-26 23:18:53 +0530

    Time: 3 ms (49.25%), Space: 43.7 MB (36.46%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -3,22 +3,19 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> powerSet = new HashSet<>();
+        List<List<Integer>> powerSet = new ArrayList<>();
         powerSet.add(new ArrayList<Integer>());
-        getSubsets(nums, powerSet, new ArrayList<Integer>(), 0, nums.length);
-        List<List<Integer>> powerList=new ArrayList<>();
-        powerList.addAll(powerSet);
-        // for(List<Integer> x: powerSet) {
-        //     powerList.add(x);
-        // }
-        return powerList;
+        getSubsets(nums, powerSet, new ArrayList<Integer>(), 0);
+
+        return powerSet;
     }
     
-    void getSubsets(int[] nums, Set<List<Integer>> powerSet, List<Integer> list, int lastIndex, int len) {
+    void getSubsets(int[] nums, List<List<Integer>> powerSet, List<Integer> list, int lastIndex) {
         for(int i=lastIndex;i<nums.length;i++) {
+            if(i>lastIndex && nums[i] == nums[i-1]) continue;
             list.add(nums[i]);
             powerSet.add(new ArrayList<>(list));
-            getSubsets(nums, powerSet, list,i+1,len);
+            getSubsets(nums, powerSet, list,i+1);
             list.remove(list.size()-1);
         }
     }
