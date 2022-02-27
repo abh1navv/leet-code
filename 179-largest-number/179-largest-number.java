1049e84a5dff2abe1f073257827518bc090b215a
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-02-27 18:05:07 +0530

    Time: 18 ms (15.86%), Space: 43.6 MB (38.01%) - LeetHub

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -3,13 +3,7 @@ class Solution {
         List<String> list = new ArrayList<>();
         
         for(int i:nums) list.add(String.valueOf(i));
-        list.sort(new Comparator<String>(){
-                        public int compare(String a, String b){
-                            String s1 = a + b;
-                            String s2 = b + a;
-                            return s2.compareTo(s1);
-                        }
-                    });
+        list.sort((a,b) -> (b+a).compareTo(a+b));
         
         String ans = String.join("", list);
         int len = ans.length();
