7f64c8b40a05c9dddbccb53e44f5b37a0579268a
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-01 12:00:18 +0530

    Time: 9 ms (68.56%), Space: 56.6 MB (47.52%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -1,46 +1,46 @@
 public class Solution {
-  
-  public ListNode sortList(ListNode head) {
-    if (head == null || head.next == null)
-      return head;
-        
-    ListNode prev = null, slow = head, fast = head;
-    
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      slow = slow.next;
-      fast = fast.next.next;
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode prev = null, slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        return merge(left, right);
     }
-    
-    prev.next = null;
-    
-    ListNode left = sortList(head);
-    ListNode right = sortList(slow);
-    
-    return merge(left, right);
-  }
   
-  ListNode merge(ListNode left, ListNode right) {
-    ListNode sorted = new ListNode(0), temp = sorted;
-    
-    while (left != null && right != null) {
-      if (left.val < right.val) {
-        sorted.next = left;
-        left = left.next;
-      } else {
-        sorted.next = right;
-        right = right.next;
-      }
-      sorted = sorted.next;
+    ListNode merge(ListNode left, ListNode right) {
+        ListNode sorted = new ListNode(0), temp = sorted;
+
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                sorted.next = left;
+                left = left.next;
+            } else {
+                sorted.next = right;
+                right = right.next;
+            }
+            sorted = sorted.next;
+        }
+
+        if (left != null)
+          sorted.next = left;
+
+        if (right != null)
+          sorted.next = right;
+
+        return temp.next;
     }
-    
-    if (left != null)
-      sorted.next = left;
-    
-    if (right != null)
-      sorted.next = right;
-    
-    return temp.next;
-  }
 
 }
