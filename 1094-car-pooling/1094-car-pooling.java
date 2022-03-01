45bf67063c26679b5eaed0a6964b406b9daa6d04
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-01 09:05:52 +0530

    Time: 11 ms (28.64%), Space: 45 MB (7.82%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,15 +1,31 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int in_car = 0;
-        int[] increase = new int[1001];
-        for (int i = 0; i < trips.length; i ++) { // find out the number of the **net increase** passengers for each stop
-            increase[trips[i][1]] += trips[i][0];
-            increase[trips[i][2]] -= trips[i][0];
+        int currentCap = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.station - b.station);
+        
+        for(int[] trip:trips) {
+            pq.offer(new Pair(trip[1], trip[0]));
+            pq.offer(new Pair(trip[2], -trip[0]));
         }
-        for (int i = 0; i <= 1000; i ++) { // from start to end, for each stop we calculate the number of passengers in the car.
-            in_car += increase[i];
-            if (in_car > capacity) return false; // once the number of passengers in the car exceeds the capacity
+        
+        while(!pq.isEmpty()) {
+            Pair current = pq.poll();
+            currentCap+=current.capacity;
+            if(currentCap > capacity && current.station != pq.peek().station){
+                return false;
+            }
         }
+        
         return true;
+    }
+    
+    class Pair {
+        int capacity;
+        int station;
+        
+        Pair(int station, int capacity) {
+            this.capacity = capacity;
+            this.station = station;
+        }
     }
 }
