b7d32098893d5bd2d31e75b95f3bdf819cd87c20
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-02 22:34:47 +0530

    Time: 11 ms (23.42%), Space: 43.7 MB (11.36%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -13,9 +13,8 @@ class Solution {
     }
     
     boolean checkIL(String s1, int l1, String s2, int l2, String s3, int l3) {
-        String key = l1+""+l2+""+l3;
+        String key = l1+""+l2;
         if(cache.containsKey(key)) {
-            //System.out.println("Cache hit");
             return cache.get(key);
         }
         
