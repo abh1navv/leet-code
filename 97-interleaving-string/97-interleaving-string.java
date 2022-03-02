7c984ae6c25d0f3cde72381271263b9933f31737
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-02 22:33:24 +0530

    Time: 8 ms (38.45%), Space: 42.2 MB (38.35%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -4,6 +4,10 @@ class Solution {
         int l1 = s1.length()-1;
         int l2 = s2.length()-1;
         int l3 = s3.length()-1;
+        
+        if(l1+l2 != l3-1) {
+            return false;
+        }
 
         return checkIL(s1, l1, s2, l2, s3, l3);
     }
@@ -15,28 +19,25 @@ class Solution {
             return cache.get(key);
         }
         
-        while((l1>=0 || l2>=0) && l3>=0) {
-            char curr = s3.charAt(l3);
-            char curr1 = l1>=0 ? s1.charAt(l1): 'X';
-            char curr2 = l2>=0 ? s2.charAt(l2): 'X';
-            if(curr == curr1 && curr != curr2) {
-                l1--;
-                l3--;
-            } else if(curr != curr1 && curr == curr2) {
-                l2--;
-                l3--;
-            } else if(curr != curr1 && curr != curr2) {
-                cache.put(key, false);
-                return false;
-            } else {
-                boolean first = checkIL(s1, l1-1, s2, l2, s3, l3-1); 
-                boolean second = checkIL(s1, l1, s2, l2-1, s3, l3-1);
-                cache.put(key, first || second);
-
-                return first || second;
-            }
+        if(l3<0) return true;
+        
+        boolean result = false;
+        
+        char curr = s3.charAt(l3);
+        char curr1 = l1>=0 ? s1.charAt(l1): 'X';
+        char curr2 = l2>=0 ? s2.charAt(l2): 'X';
+        if(curr == curr1 && curr != curr2) {
+            result = checkIL(s1, l1-1, s2, l2, s3, l3-1);
+        } else if(curr != curr1 && curr == curr2) {
+            result = checkIL(s1, l1, s2, l2-1, s3, l3-1);
+        } else if(curr != curr1 && curr != curr2) {
+            result = false;
+        } else {
+            boolean first = checkIL(s1, l1-1, s2, l2, s3, l3-1); 
+            boolean second = checkIL(s1, l1, s2, l2-1, s3, l3-1);
+            result = first || second;
         }
-        boolean result = l1 == -1 && l2 == -1 && l3 == -1;
+        
         cache.put(key, result);
         return result;
     }
