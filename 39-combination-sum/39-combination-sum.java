3f196f3733f6bec4aaf0961d95d439609f98f79c
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-03 14:12:31 +0530

    Time: 29 ms (7.06%), Space: 48.9 MB (8.68%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Set<List<Integer>> ans = new HashSet<>();
+        List<Integer> current = new LinkedList<>();
+        for(int i=0; i< candidates.length; i++) {
+            getCombinations(candidates, target, ans, current, i);
+        }
+        List<List<Integer>> list = new ArrayList<>();
+        for(List<Integer> ans1: ans) {
+            list.add(ans1);
+        }
+        return list;
+    }
+    
+    public void getCombinations(int[] candidates, int target, Set<List<Integer>> ans, 
+                           List<Integer> current, int index) {
+        if(target == 0) {
+            ans.add(new ArrayList<Integer>(current));
+        } else if(target < 0) {
+            return;
+        }
+        current.add(candidates[index]);
+        target -= candidates[index];
+        for(int i=index; i< candidates.length; i++) {
+            getCombinations(candidates, target, ans, current, i);
+        }
+        current.remove(current.size()-1);
+
+    }
+}
