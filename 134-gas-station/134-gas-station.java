9531e8ab8563390e2b427d693d0d6dd0353bba9d
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-03 12:50:31 +0530

    Time: 1 ms (99.97%), Space: 62.3 MB (77.51%) - LeetHub

---
+++ 134-gas-station/134-gas-station.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int extra = 0;
+        int tank = 0;
+        int start = 0;
+        
+        for(int i=0;i<gas.length;i++) {
+            tank += gas[i] - cost[i];
+            extra += gas[i] - cost[i];
+            
+            if(extra < 0) {
+                start = i+1;
+                extra = 0;
+            }
+        }
+        
+        return tank >= 0 ? start : -1;
+    }
+    
+    
+
+}
