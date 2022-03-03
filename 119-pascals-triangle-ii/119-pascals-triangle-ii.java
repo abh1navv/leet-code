57def58ef2d744858b07bed3e35fd4946f2217c0
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-03 10:15:53 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (52.31%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -1,13 +1,14 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        int[] current = new int[rowIndex+1];
+        Integer[] current = new Integer[rowIndex+1];
         current[0] = 1;
         for(int i=1; i<rowIndex+1;i++){
+            current[i] = 0;
             for(int j=i; j>=1;j--) {
                 current[j] += current[j-1];
             }
         }
         
-        return Arrays.stream(current).boxed().collect(Collectors.toList());
+        return Arrays.asList(current);
     }
 }
