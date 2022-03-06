b6e3cc8a8a82b4c2bfa698289aeb03cec77aaf97
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-06 15:51:51 +0530

    Time: 2 ms (100.00%), Space: 50.2 MB (89.62%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,41 +1,49 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
+        //if (points==null || points.length==0 || k<=0) 
+        //    return new int[0][0];
         if (points.length<=k) 
             return points;
-        quickSelect(points, k, 0 , points.length-1);
+        
+        quickSelect(points, k, 0, points.length-1);
         return Arrays.copyOfRange(points, 0, k);
     }
     
-    void quickSelect(int[][] points, int k, int start, int end) {
-        if(start>=end) return;
-       
-        int j = partition(points, k, start, end);
-        
-        if(j == k) return;
-        else if(j<k) quickSelect(points, k, j+1 , end);
-        else quickSelect(points, k, start, j-1);
-    }
-    
-    int partition(int[][] points, int k, int start, int end) {
-        int[] current = points[end];
-        int j=start;
-        for(int i=start; i<end; i++) {
-            if(distance(points[i][0], points[i][1]) <= distance(current[0], current[1])) {
-                int[] temp = points[j];
-                points[j] = points[i];
-                points[i] = temp;
-                j++;
+    private void quickSelect(int[][] points, int k, int left, int right) {
+        if (left==right) 
+            return;
+        int i = left;
+        int j = right;
+        int pivot = left + ((right-left)/2);
+        int pivotD = distance(points[pivot]);
+        while(left <= right) {
+            int leftD = distance(points[left]);
+            int rightD = distance(points[right]);
+            if(leftD < pivotD) 
+                left++;
+            else if(rightD>pivotD) 
+                right--;
+             else {
+                swap(points, left, right);
+                left++;
+                right--;
             }
         }
         
-        int[] temp = points[end];
-        points[end] = points[j];
-        points[j] = temp;
+        if (left < k) 
+            quickSelect(points, k, left, j);
+        else if (left>k) 
+            quickSelect(points, k, i, left-1);
         
-        return j;
     }
     
-    int distance(int x, int y) {
-        return x*x + y*y;
+    private int distance(int[] point) {
+        return point[0]*point[0] + point[1]*point[1];
+    }
+    
+    private void swap(int[][] points, int i, int j) {
+        int[] tmp = points[i];
+        points[i] = points[j];
+        points[j] = tmp;
     }
 }
