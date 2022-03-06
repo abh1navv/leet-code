f8f71eafcacbb14589dc8766abfb877a330ef16a
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-06 16:15:51 +0530

    Time: 16 ms (54.31%), Space: 53.7 MB (40.53%) - LeetHub

---
+++ 561-array-partition-i/561-array-partition-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i=0; i< nums.length/2;i++) {
+            sum+=nums[i*2];
+        }
+        return sum;
+    }
+}
