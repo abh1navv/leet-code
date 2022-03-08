98f79ef9205e5fcf0418e3e09d2976e717da68cb
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-08 11:59:01 +0530

    Time: 33 ms (8.05%), Space: 50 MB (57.34%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.java
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.java
@@ -14,12 +14,4 @@ class Solution {
         }
         return min;
     }
-    
-    public int getInversions(String s) {
-        int count = 0;
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '0') count++;
-        }
-        return count;
-    }
 }
