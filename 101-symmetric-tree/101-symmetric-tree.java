923774a37637bf3825118f29523e02556f66a0dd
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-08 22:21:04 +0530

    Time: 1 ms (59.58%), Space: 42.6 MB (20.53%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -16,19 +16,29 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null || (root.left == null && root.right == null)) return true;
-        
         else if(root.left == null) return false;
         
-        return preOrder(root.left, true).equals(preOrder(root.right, false));
+        StringBuilder left = new StringBuilder();
+        StringBuilder right = new StringBuilder();
+        preOrder(root.left, true, left);
+        preOrder(root.right, false, right);
+        
+        return left.toString().equals(right.toString());
     }
     
-    public String preOrder(TreeNode root, boolean isLeft) {
-        if(root == null) return null;
-        
+    public void preOrder(TreeNode root, boolean isLeft, StringBuilder sb) {
+        if(root == null) {
+            sb.append("X");
+            return;
+        }
         if(isLeft) {
-            return root.val + preOrder(root.left, isLeft) + preOrder(root.right, isLeft);
+            sb.append(root.val);
+            preOrder(root.left, isLeft,sb);
+            preOrder(root.right, isLeft,sb);
         } else {
-            return root.val + preOrder(root.right, isLeft) + preOrder(root.left, isLeft);
+            sb.append(root.val);
+            preOrder(root.right, isLeft,sb);
+            preOrder(root.left, isLeft,sb);
         }
     }
 }
