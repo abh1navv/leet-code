b5b515cbe365cd5cbe7e47ef551435c2c3f33c5f
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-08 09:08:02 +0530

    Time: 12 ms (58.96%), Space: 47.5 MB (32.73%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -6,21 +6,16 @@ class Solution {
         int count = nums[0];
         for(int i=1; i<prefix.length; i++) {
             prefix[i] = count + prefix[i-1];
-            if(nums[i] == 1) count++;
+            if(boxes.charAt(i) == '1') count++;
         }
-        //System.out.println(Arrays.toString(prefix));
         count = nums[nums.length-1];
         nums[nums.length-1] = 0;
 
         for(int i=nums.length-2; i>=0; i--) {
-            //nums[i] = count + prefix[i];
-            //prefix[i] += nums[i];
             nums[i] = nums[i+1] + count;
             prefix[i] += nums[i];
             if(boxes.charAt(i) == '1') count++;
         }
-        //System.out.println(Arrays.toString(nums));
-
         
         return prefix;
     }
