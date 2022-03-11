038fc5e284c30b8649be09e53a5401d80e3ed5cb
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-11 10:40:46 +0530

    Time: 20 ms (9.84%), Space: 49.3 MB (5.05%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,48 @@
+import java.util.Collection;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Set<TreeNode> pathP = new HashSet<>();
+        findPath(root, p,new ArrayList<TreeNode>(), pathP);
+        List<TreeNode> pathQ = new ArrayList<>();
+        findPath(root,  q,new ArrayList<TreeNode>(), pathQ);
+        
+        //System.out.println(pathP.stream().map(i->String.valueOf(i.val)).collect(Collectors.joining(",")));
+        //System.out.println(pathQ.stream().map(i->String.valueOf(i.val)).collect(Collectors.joining(",")));
+        
+        for(int i=pathQ.size()-1; i>=0; i--) {
+            if(pathP.contains(pathQ.get(i))) {
+                return pathQ.get(i);
+            }
+        }
+        
+        return root;
+        
+    }
+    
+    public boolean findPath(TreeNode root, TreeNode target, List<TreeNode> current, Collection<TreeNode> path) {
+        if(root == null) {
+            return false;
+        }
+        current.add(root);
+
+        if(root == target) {
+            path.addAll(current);
+            return true;
+        }
+        
+        boolean ans = findPath(root.left, target, current, path) || findPath(root.right, target,current, path);
+        if(!ans) {
+            current.remove(current.size()-1);
+        }
+        return ans;
+    }
+}
