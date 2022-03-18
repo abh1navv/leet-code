288f0a9777f652d443da169c79c87b8f54654830
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-18 10:58:26 +0530

    Time: 4 ms (75.34%), Space: 43.3 MB (25.30%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -9,16 +9,17 @@ class Solution {
         boolean[] visited=new boolean[26];
         Stack<Character> stack = new Stack<>();
         
-        
+        int index = 0;
         for(char c: arr) {
-            count[c-'a']--;
-            if(!visited[c-'a']) {
+            index = c-'a';
+            count[index]--;
+            if(!visited[index]) {
                 while(!stack.isEmpty() && stack.peek() > c && count[stack.peek()-'a'] !=0) {
                     visited[stack.pop()-'a'] = false;
                 }
 
-                visited[c-'a'] = true;
-                stack.push((char)c);
+                visited[index] = true;
+                stack.push(c);
             }
         }
         
