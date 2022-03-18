4ed892feffe6148d28d5757ef77e7bc1d1979f5f
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-18 10:57:32 +0530

    Time: 10 ms (19.55%), Space: 43.5 MB (14.53%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -1,13 +1,16 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] count = new int[26];        
-        s.chars().forEach(c->count[c-'a']++);
+        int[] count = new int[26];     
+        char[] arr = s.toCharArray();
+        for(char c: arr) {
+            count[c-'a']++;
+        }
         
         boolean[] visited=new boolean[26];
         Stack<Character> stack = new Stack<>();
         
         
-        s.chars().forEach(c->{
+        for(char c: arr) {
             count[c-'a']--;
             if(!visited[c-'a']) {
                 while(!stack.isEmpty() && stack.peek() > c && count[stack.peek()-'a'] !=0) {
@@ -17,7 +20,7 @@ class Solution {
                 visited[c-'a'] = true;
                 stack.push((char)c);
             }
-        });
+        }
         
         StringBuilder sb =new StringBuilder();
         
