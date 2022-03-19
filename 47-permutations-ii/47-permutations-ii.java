63b2bc44adcf899ec561c168bd19ff3a3803c312
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-19 18:54:16 +0530

    Time: 96 ms (11.41%), Space: 54.5 MB (11.32%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -2,23 +2,24 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         Set<List<Integer>> ans = new HashSet<>();
         
-        Set<Integer> seenIndexes = new HashSet<>();
+        boolean[] seenIndexes = new boolean[nums.length];
         getPermutations(new ArrayList<>(), ans, nums, seenIndexes);
 
         return new ArrayList(ans);
     }
     
-    public void getPermutations(List<Integer> current, Set<List<Integer>> ans, int[] nums, Set<Integer> seenIndexes) {
+    public void getPermutations(List<Integer> current, Set<List<Integer>> ans, int[] nums, boolean[] seenIndexes) {
         if(current.size() == nums.length) {
             ans.add(new ArrayList<>(current));
         }       
         
         for(int i=0; i<nums.length; i++) {
-            if(seenIndexes.add(i)) {
+            if(!seenIndexes[i]) {
+                seenIndexes[i] = true;
                 current.add(nums[i]);
                 getPermutations(current, ans, nums, seenIndexes);
                 current.remove(current.size()-1);
-                seenIndexes.remove(i);
+                seenIndexes[i] = false;
             }
         }
     }
