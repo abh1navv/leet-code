31768046453524abf965aa270804709b5f2aa0f5
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-25 09:59:08 +0530

    Time: 4 ms (21.03%), Space: 42.3 MB (40.68%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -6,9 +6,10 @@ class Solution {
         int first = 0,second=0;
         int sum = 0, n=costs.length/2;
         int index = 0;
+        int[] curr;
         
         while(first!=n && second!=n) {
-            int[] curr = costs[index++];
+            curr = costs[index++];
             if(curr[0]> curr[1]) {
                 sum+=curr[1];
                 second++;
@@ -19,29 +20,16 @@ class Solution {
         }
         
         while(first!=n) {
-            int[] curr = costs[index++];
+            curr = costs[index++];
             sum+=curr[0];
             first++;
         }
         while(second!=n) {
-            int[] curr = costs[index++];
+            curr = costs[index++];
             sum+=curr[1];
             second++;
         }
         
         return sum;
-    }
-    
-    class Cost {
-        int first;
-        int second;
-        Cost(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-        
-        public String toString() {
-            return first + ":" +second;
-        }
     }
 }
