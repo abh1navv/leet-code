6a498a14a2ad2cf3d8b7d9afb8e97a4b8dbe5d1a
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-25 09:55:24 +0530

    Time: 3 ms (45.38%), Space: 42.1 MB (54.01%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -2,14 +2,12 @@ class Solution {
     public int twoCitySchedCost(int[][] costs) {
         PriorityQueue<Cost> pqFirst = new PriorityQueue<>(
             (a,b)->Math.abs(b.first-b.second)-Math.abs(a.second-a.first));
-        //PriorityQueue<Cost> pqSec = new PriorityQueue<>((a,b)->a.second-b.second);
+
         for(int i=0; i<costs.length; i++) {
             Cost cost = new Cost(costs[i][0], costs[i][1]);
             pqFirst.offer(cost);
-            //pqSec.offer(cost);
         }
         
-        Set<Cost> seen = new HashSet<>();
         int first = 0,second=0;
         int sum = 0, n=costs.length/2;
         
@@ -34,26 +32,6 @@ class Solution {
             sum+=curr.second;
             second++;
         }
-        
-        
-        
-        // while(n!=0) {
-        //     while(seen.contains(pqFirst.peek())) {
-        //         pqFirst.poll();
-        //     }
-        //     Cost curr = pqFirst.poll();
-        //     sum+=curr.first;
-        //     seen.add(curr);
-        //     while(seen.contains(pqSec.peek())) {
-        //         pqSec.poll();
-        //     }
-        //     curr = pqSec.poll();
-        //     sum+=curr.second;
-        //     seen.add(curr);
-        //     n--;
-        // }
-        
-        
         
         return sum;
     }
