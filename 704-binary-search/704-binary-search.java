2ab96300af97332cd8f76d6d4d45e54474c19009
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-26 09:06:55 +0530

    Time: 0 ms (100.00%), Space: 54 MB (65.97%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0,end=nums.length-1,mid=0;
+        while(start<=end) {
+            mid = (start+end)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) start=mid+1;
+            else end=mid-1;
+        }
+        
+        return -1;
+    }
+}
