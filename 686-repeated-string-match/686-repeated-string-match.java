7dda751c057325b6330d976361c359c750390dc9
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-26 15:47:39 +0530

    Time: 1109 ms (5.09%), Space: 42.4 MB (72.87%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,9 +1,10 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         int alen = a.length(), matches=-1;
+        int blen = b.length();
         for(int i=0;i <alen; i++) {
             if(a.charAt(i) == b.charAt(0)) {
-                matches = getRepetitions(a, b, i, alen);
+                matches = getRepetitions(a, b, i, alen, blen);
                 if(matches != -1) return matches+1;
             }
         }
@@ -11,8 +12,8 @@ class Solution {
         return matches;
     }
     
-    public int getRepetitions(String a, String b, int start, int n) {
-        for(int i=0; i<b.length(); i++) {
+    public int getRepetitions(String a, String b, int start, int n, int blen) {
+        for(int i=0; i<blen; i++) {
             if(b.charAt(i)!=a.charAt(start%n)) {
                 return -1;
             }
