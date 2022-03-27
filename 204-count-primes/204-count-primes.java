d36a8b7e8f83df334e39e3c406afa3a824c45968
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-27 15:58:33 +0530

    Time: 210 ms (32.25%), Space: 68.9 MB (28.39%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -6,8 +6,9 @@ class Solution {
         for(int i=2; i<n;i++) {
             if(!isNotPrime[i]) {
                 count++;
-                for(int j=2;i*j<n;j++) {
-                    isNotPrime[i*j] = true;
+                int j=2;
+                while(i*j<n) {
+                    isNotPrime[i*j++] = true;
                 }
             }
         }
