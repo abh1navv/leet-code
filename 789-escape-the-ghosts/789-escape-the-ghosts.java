bc0db0135bb6116f21f43e175d0e6948a56b8777
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-30 09:56:01 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (48.18%) - LeetHub

---
+++ 789-escape-the-ghosts/789-escape-the-ghosts.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int dist = Math.abs(target[0])+Math.abs(target[1]);
+        
+        for(int[] ghost: ghosts) {
+            if(Math.abs(ghost[0]-target[0])+Math.abs(ghost[1]-target[1]) <= dist) return false;
+        }
+        
+        return true;
+    }
+}
