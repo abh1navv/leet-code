4caa9e7640a5cbc2ad49fa886cbac3832c77e16e
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-03-30 10:02:34 +0530

    Time: 2 ms (28.47%), Space: 42 MB (79.56%) - LeetHub

--- 789-escape-the-ghosts/789-escape-the-ghosts.java
+++ 789-escape-the-ghosts/789-escape-the-ghosts.java
@@ -1,8 +1,9 @@
 class Solution {
     public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int dist = Math.abs(target[0])+Math.abs(target[1]);
         return Arrays.stream(ghosts)
             .noneMatch((ghost)->
                        Math.abs(ghost[0]-target[0])+Math.abs(ghost[1]-target[1]) 
-                       <= Math.abs(target[0])+Math.abs(target[1]));
+                       <= dist);
     }
 }
