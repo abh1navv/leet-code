9335f8b3e3e2b0cd1ca950009ff2170c4d40e537
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-04-19 17:16:03 +0530

    Time: 12 ms (75.69%), Space: 69.7 MB (40.33%) - LeetHub

---
+++ 491-increasing-subsequences/491-increasing-subsequences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        backtrack(nums, 0, curr, res);
+        return res;
+    }
+    
+    void backtrack(int[] nums, int start, List<Integer> curr, List<List<Integer>> res) {
+        if(curr.size() > 1) {
+            res.add(new ArrayList<>(curr));
+        }
+        Set<Integer> checked = new HashSet<>();
+
+        for(int i=start; i<nums.length; i++) {
+            if(checked.contains(nums[i])) continue;
+            if(curr.isEmpty() || nums[i]>=curr.get(curr.size()-1)) {
+                checked.add(nums[i]);
+                curr.add(nums[i]);
+                backtrack(nums, i+1, curr, res);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+}
