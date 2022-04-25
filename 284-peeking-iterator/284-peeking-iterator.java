5afd49c66b699eb178b84385af24f062c097cbf8
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-04-25 09:27:32 +0530

    Time: 6 ms (61.79%), Space: 42.9 MB (42.94%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.java
@@ -0,0 +1,31 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+class PeekingIterator implements Iterator<Integer> {
+    
+    List<Integer> list = new LinkedList<>();
+    int pointer = 0;
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    while(iterator.hasNext()) {
+            list.add(iterator.next());
+        }
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        return list.get(pointer);
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    return list.get(pointer++);
+	}
+	
+	@Override
+	public boolean hasNext() {
+	    return pointer<list.size();
+	}
+}
