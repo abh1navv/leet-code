d4ba9c08c4ef0dea6e9b9f733bf8b4be6d13d8e8
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-05-17 21:55:08 +0530

    Time: 1 ms (46.39%), Space: 43.1 MB (69.37%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -18,7 +18,7 @@ class Solution {
         if(root == null) return false;
         
         targetSum -= root.val;
-        if(targetSum == 0 && root.left == null && root.right == null) return true;
+        if(root.left == null && root.right == null) return targetSum == 0;
         
         return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
     }
