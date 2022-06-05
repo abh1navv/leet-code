ab59ee77f0369c13137eec8898c4387c5ea4ed07
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-06-05 10:13:06 +0530

    Time: 33 ms (6.29%), Space: 47 MB (5.03%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -40,9 +40,6 @@ class Solution {
     boolean[][] copyMatrix() {
         boolean[][] copy = new boolean[n][n];
         for(int i=0; i<n; i++) {
-            // for(int j=0; j<n; j++) {
-            //     copy[i][j] = matrix[i][j];
-            // }
             copy[i] = matrix[i].clone();
         }
         return copy;
