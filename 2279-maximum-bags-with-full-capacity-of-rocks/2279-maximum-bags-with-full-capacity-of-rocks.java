fe4bb0468035eabc354e57938b558c1e351dd2f3
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-07-01 12:14:43 +0530

    Time: 95 ms (24.42%), Space: 96.5 MB (29.01%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,7 +1,9 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        PriorityQueue<Integer> pq = preprocess(capacity, rocks);
-        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<capacity.length; i++) {
+            pq.offer(capacity[i]-rocks[i]);
+        }        
         int ans = 0;
         while(!pq.isEmpty() && pq.peek() <= additionalRocks) {
             ans++;
@@ -11,15 +13,5 @@ class Solution {
         return ans;
     }
     
-    PriorityQueue<Integer> preprocess(int[] capacity, int[] rocks) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=0; i<capacity.length; i++) {
-            //if(capacity[i]-rocks[i]>0) {
-                pq.offer(capacity[i]-rocks[i]);
-            //}
-        }
-        
-        return pq;
-    }
     
 }
