37c08c18c206e3cd0dca4beb92d6fdd65005df78
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-08-28 10:59:08 +0530

    Time: 2 ms (51.95%), Space: 43.9 MB (86.52%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> visited=new HashSet<>();
+        List<Integer> queue = new ArrayList<>();
+        queue.add(0);
+        int visitedCount = 0;
+        
+        int start=0;
+        
+        while(start < queue.size()) {
+            if(!visited.contains(queue.get(start))) {
+                queue.addAll(rooms.get(queue.get(start)));
+                visited.add(queue.get(start));
+                visitedCount++;
+            }
+            start++;
+        }
+        
+        return visitedCount == rooms.size();
+    }
+}
