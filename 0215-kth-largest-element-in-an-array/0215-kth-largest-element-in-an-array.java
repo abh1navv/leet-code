14908843d4f618f2690e76cade34441ab6d4b0e6
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-08-29 12:32:12 +0530

    Time: 2202 ms (5.07%), Space: 58 MB (43.70%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -4,27 +4,29 @@ class Solution {
         return nums[nums.length-k];
     }
     
-    public void quickSelect(int start, int end, int[] nums, int k) {
-        if(start>=end) return;
-        
-        int pivot = nums[start];
-        int j=start+1;
-        for(int i=start+1; i<=end; i++) {
-            if(nums[i] < pivot) {
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                j++;
-            } 
-        }
+    public void quickSelect(int start, int end, int[] nums, int k) {        
+        while(true) {
+            int pivot = nums[start];
+            int j=start+1;
+            for(int i=start+1; i<=end; i++) {
+                if(nums[i] < pivot) {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    j++;
+                } 
+            }
 
-        int temp = nums[j-1];
-        nums[j-1] = pivot;
-        nums[start] = temp;
-        
-        if(j-1 > k) 
-            quickSelect(start, j-2, nums,k);
-        else if(j-1 < k)
-            quickSelect(j, end, nums,k);
+            int temp = nums[j-1];
+            nums[j-1] = pivot;
+            nums[start] = temp;
+
+            if(j-1 > k) 
+                end = j-2;
+            else if(j-1 < k)
+                start=j;
+            else 
+                return;
+        }
     }
 }
