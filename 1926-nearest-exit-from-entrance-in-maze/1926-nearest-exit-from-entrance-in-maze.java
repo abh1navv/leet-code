11861fc5230834965a52785896a3e15fd56e7f19
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-08-29 10:15:06 +0530

    Time: 4 ms (96.37%), Space: 45.3 MB (95.37%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,12 +1,10 @@
 class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
-        Set<String> visited = new HashSet<>();
-        boolean newCellsFound = true;
         int minDepth = 0;
         
         List<Pair> queue=new ArrayList<>();
         queue.add(new Pair(entrance[0], entrance[1]));
-        visited.add(entrance[0]+"_"+entrance[1]);
+        maze[entrance[0]][entrance[1]] = '+';
         
         int index = 0;
         int row, col;
@@ -18,38 +16,39 @@ class Solution {
                 Pair pair = queue.get(index);
                 row = pair.a;
                 col = pair.b;
-                if(!visited.contains((row-1)+"_"+col) && row > 0 && maze[row-1][col] == '.') {
+                if(row > 0 && maze[row-1][col] == '.') {
                     if(row-1 == 0 || col==0 || col==maze[0].length-1) {
                         return minDepth;
                     } else {
                         queue.add(new Pair(row-1, col));
-                        visited.add((row-1)+"_"+col);
+                       maze[row-1][col] = '+';
+
                     }
                 } 
-                if(!visited.contains((row+1)+"_"+col) && row < maze.length-1 && maze[row+1][col] == '.') {
+                if(row < maze.length-1 && maze[row+1][col] == '.') {
                     if(row+1 == maze.length-1 || col==0 || col==maze[0].length-1) {
                         return minDepth;
                     } else {
                         queue.add(new Pair(row+1, col));
-                                visited.add((row+1)+"_"+col);
+                                maze[row+1][col] = '+';
 
                     }
                 } 
-                if(!visited.contains(row+"_"+(col-1)) && col > 0 && maze[row][col-1] == '.') {
+                if(col > 0 && maze[row][col-1] == '.') {
                     if(col-1 == 0 || row==0 || row==maze.length-1) {
                         return minDepth;
                     } else {
                         queue.add(new Pair(row, col-1));
-                                visited.add(row+"_"+(col-1));
+                                maze[row][col-1] = '+';
 
                     }
                 } 
-                if(!visited.contains(row+"_"+(col+1)) && col < maze[0].length-1 && maze[row][col+1] == '.') {
+                if(col < maze[0].length-1 && maze[row][col+1] == '.') {
                     if(col+1 ==  maze[0].length-1 || row==0 || row==maze.length-1) {
                         return minDepth;
                     } else {
                         queue.add(new Pair(row, col+1));
-                        visited.add(row+"_"+(col+1));
+                        maze[row][col+1] = '+';
 
                     }
                 }
