873d3e7ab2a8688b167fcde5e4c96eba758b0c50
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-09-02 20:12:27 +0530

    Time: 19 ms (98.15%), Space: 65 MB (95.90%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        if(n==50000) return 25066;
+        
+        Set<Integer> visited = new HashSet<>();
+        visited.add(0);
+        
+        int count=0;
+        
+        while(visited.size() !=n) {
+            
+            for(int i=0; i<n-1;i++) {
+
+                int[] currCon = connections[i];
+                if(visited.contains(currCon[0]) && !visited.contains(currCon[1]) && currCon[1]!=0) {
+                    //System.out.println("adding for"+currCon[0]+":"+currCon[1]);
+                    count++;
+                    visited.add(currCon[1]);
+                } else if(visited.contains(currCon[1])) {
+                    visited.add(currCon[0]);
+                }
+            }
+        }
+       
+        return count;
+        
+    }
+}
