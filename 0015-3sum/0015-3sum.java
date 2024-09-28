0af18ba07985fe9e68a99fc43ecd86c8860cb318
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-09-28 14:27:49 +0530

    Time: 34 ms (38.24%), Space: 51.2 MB (86.88%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,8 +2,6 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         
-        
-        //Set<List<Integer>> ans = new HashSet<>();
         List<List<Integer>> ans = new ArrayList<>();
         
         for(int i=0; i<nums.length-2; i++) {
@@ -30,7 +28,6 @@ class Solution {
             }
         }
         
-        //return new ArrayList<>(ans);
         return ans;
     }
     
