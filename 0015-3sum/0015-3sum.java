5d2ee67a31bf91a737fcc87e600b9638eaefe824
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-09-28 14:14:51 +0530

    Time: 35 ms (36.98%), Space: 51.1 MB (93.66%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,17 +7,13 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         
         for(int i=0; i<nums.length-2; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+            while(i > 0 && i<nums.length-2 && nums[i] == nums[i-1]) i++;
 
             int j=i+1;
             int k= nums.length-1;
             
             
             while(j<k) {
-                //while(j< k && nums[j] == nums[j+1]) j++;
-                //while(k >j && nums[k] == nums[k-1]) k--;
-                
-                //System.out.println(List.of(nums[i], nums[j], nums[k]));
                 
                 if(nums[j] + nums[k] == -1*nums[i]) {
                     ans.add(List.of(nums[i], nums[j], nums[k]));
