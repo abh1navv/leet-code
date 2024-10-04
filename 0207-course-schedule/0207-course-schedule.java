09df610df3345f8ae8936ad22cb364e4d0869b46
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-04 21:45:32 +0530

    Time: 9 ms (20.37%), Space: 45.7 MB (26.11%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] required = new int[numCourses];
+        Map<Integer, List<Integer>> dependents = new HashMap<>();
+        
+        for(int[] pair: prerequisites) {
+            required[pair[0]] ++;
+            var list = dependents.getOrDefault(pair[1], new ArrayList<>());
+            list.add(pair[0]);
+            dependents.put(pair[1], list);
+        }
+        
+        Queue<Integer> next = new LinkedList<>();
+        
+        for(int i=0; i<numCourses; i++) {
+            if(required[i] == 0) {
+                next.add(i);
+            }
+        }
+        
+        int count = 0;
+        
+        
+        while(!next.isEmpty()) {
+            //for(int n: next) {
+                int n = next.poll();
+                count++;
+                if(dependents.containsKey(n)) {
+                for(int x: dependents.get(n)) {
+                    required[x]--;
+                    if(required[x] <= 0){
+                        next.add(x);
+                    }
+                }
+                }
+            
+        }
+        
+        return count == numCourses;
+    }
+}
