6a4889be82ca5a698a842c00835072afc88ae4b8
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-05 11:29:32 +0530

    Time: 7 ms (72.48%), Space: 42 MB (99.21%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -26,16 +26,4 @@ class Solution {
         return Arrays.equals(freq1, freq2);
     }
     
-    
-    boolean isAPermutation(String s1, String s2) {
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-        
-        for(int i=0; i<s1.length(); i++) {
-            freq1[s1.charAt(i)-'a']++;
-            freq2[s2.charAt(i)-'a']++;
-        }
-        
-        return Arrays.equals(freq1, freq2);
-    }
 }
