8605122afbf77a5f523d39febf99cdc52c9cc80f
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-05 11:59:46 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (66.28%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        
+        int[][] matrix = new int[n][n];
+        
+        int row = 0, col = 0, len = n-1;
+        int nextRow = 0, nextCol = 0, start=0;
+        
+        for(int i=1; i<= n*n; i++) {
+            matrix[row][col] = i;
+            
+            nextRow = getNextRow(row, col, len, start);
+            nextCol = getNextCol(row, col, len, start);
+             if(col == start && row == start+1) {
+                 len--;
+                 start++;
+             }
+            
+            row = nextRow;
+            col = nextCol;
+            
+            //System.out.println(row +" "+col);
+        }
+        
+        return matrix;
+    }
+    
+    int getNextRow(int row, int col, int len, int start) {
+        if(col == len && row < len) return row+1; // going down
+        if(col == len && row == len) return row; // going left
+        if(col == start && row > start+1) return row-1; // going up
+        if(col == start && row == start+1) return row; // going right
+        
+        
+        return row;
+    }
+    
+    int getNextCol(int row, int col, int len, int start) {
+        if(row == start && col < len) return col+1; // going right
+        if(row == len && col == len) return col-1; // going left
+        if(row == len && col > start) return col-1; // going left
+        if(col == start && row == start) return col; // going up
+        if(col == start && row == start+1) return col+1; // going right
+        
+        return col;
+    }
+
+    
+    
+}
