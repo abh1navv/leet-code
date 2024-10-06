6c4e194ee512c4be55436aae8accead3ce6eeba9
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-06 16:22:51 +0530

    Time: 1 ms (94.89%), Space: 44.3 MB (68.36%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,21 +2,22 @@ class Solution {
     
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        findSubsets(ans, new LinkedHashSet<>(), nums);
+        findSubsets(ans, new ArrayList<>(), nums);
         
         return ans;
     }
     
-    void findSubsets(List<List<Integer>> ans, Set<Integer> curr, int[] nums) {
+    void findSubsets(List<List<Integer>> ans, List<Integer> curr, int[] nums) {
         if(curr.size() == nums.length) {
             ans.add(new ArrayList<>(curr));
             return;
         } 
 
         for(int i=0; i<nums.length; i++) {
-            if(curr.add(nums[i])){
+            if(!curr.contains(nums[i])){
+                curr.add(nums[i]);
                 findSubsets(ans, curr, nums);
-                curr.remove(nums[i]);
+                curr.remove(curr.size()-1);
             }
         }
     }
