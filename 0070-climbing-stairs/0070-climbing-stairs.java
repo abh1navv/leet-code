49cde15207cba741b0062795487d43542a6de5f0
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-06 18:01:49 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (11.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,16 @@
 class Solution {
     
-    Map<Integer, Integer> cache = new HashMap<>();
     public int climbStairs(int n) {
-        if(cache.containsKey(n)) return cache.get(n);
-        if(n <= 1) return 1;
-        int total = climbStairs(n-1) + climbStairs(n-2);
+        if(n==1) return n;
+
+        int prev = 1, prevPrev = 1;
         
-        cache.put(n, total);
-        return total;
+        for(int i=2; i<n; i++) {
+            int temp = prevPrev+prev;
+            prevPrev = prev;
+            prev = temp;
+        }
+        
+        return prev+prevPrev;
     }
 }
