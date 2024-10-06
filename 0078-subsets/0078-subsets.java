6f1fcaa05c12e211b2f0526ec3c489a3cecddedb
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-06 16:00:28 +0530

    Time: 2 ms (54.48%), Space: 42.7 MB (33.98%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        findSubsets(ans, new ArrayList<>(), nums, 0);
+        findSubsets(ans, new HashSet<>(), nums, 0);
         
         return ans;
     }
     
-    void findSubsets(List<List<Integer>> ans, List<Integer> curr, int[] nums, int index) {
+    void findSubsets(List<List<Integer>> ans, Set<Integer> curr, int[] nums, int index) {
         ans.add(new ArrayList<>(curr));
 
         for(int i=index; i<nums.length; i++) {
             if(!curr.contains(nums[i])) {
                 curr.add(nums[i]);
                 findSubsets(ans, curr, nums, i);
-                curr.remove(curr.size()-1);
+                curr.remove(nums[i]);
             }
         }
     }
