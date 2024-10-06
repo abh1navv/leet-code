5f23ea71a9271266cc08da67466fcbd634ccd512
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-06 11:49:52 +0530

    Time: 21 ms (13.27%), Space: 49.4 MB (5.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,9 +3,7 @@ class Solution {
         Map<String, List<String>> sortAndGroup = new HashMap<>();
         
         for(String str: strs) {
-            String sorted = Stream.of(str.split(""))
-                .sorted()
-                .collect(Collectors.joining());
+            String sorted = getCountString(str);
             List<String> prev = sortAndGroup.getOrDefault(sorted, new ArrayList<>());
             prev.add(str);
             sortAndGroup.put(sorted, prev);
@@ -13,6 +11,15 @@ class Solution {
         }
         
         return sortAndGroup.values().stream().toList();
+    }
+    
+    String getCountString(String str) {
+        int[] counts = new int[26];
+        for(char ch: str.toCharArray()) {
+            counts[ch-'a']++;
+        }
+        
+        return Arrays.toString(counts);
     }
         
 }
