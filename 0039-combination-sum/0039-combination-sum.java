24bbdf0f8e50f32140dc04148d01812096a807da
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-06 16:15:42 +0530

    Time: 2 ms (83.62%), Space: 44.3 MB (92.07%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        getSubsets(nums, ans, new ArrayList<Integer>(), 0, target);
+        return ans;
+    }
+    
+    
+    void getSubsets(int[] nums, List<List<Integer>> ans, List<Integer> list, int lastIndex, int target) {
+        if(target == 0) {
+                ans.add(new ArrayList<>(list));
+                return;
+            } else if(target < 0) {
+                return;
+            }
+                
+        
+        for(int i=lastIndex;i<nums.length;i++) {
+            list.add(nums[i]);            
+            getSubsets(nums, ans, list,i, target-nums[i]);
+            list.remove(list.size()-1); 
+        }
+    }
+    
+    
+}
