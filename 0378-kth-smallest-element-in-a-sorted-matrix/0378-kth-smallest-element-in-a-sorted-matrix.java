b72ef4dc98cc209c7032b9782995d73fec3c2a1a
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-11 17:43:58 +0530

    Time: 17 ms (46.57%), Space: 47.9 MB (76.10%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -4,12 +4,12 @@ class Solution {
         
         int n = matrix.length;
         for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            pq.add(matrix[i][j]);
-            if(pq.size() > k) {
-                pq.poll();
+            for(int j=0; j<n; j++) {
+                pq.add(matrix[i][j]);
+                if(pq.size() > k) {
+                    pq.poll();
+                }
             }
-        }
         }
         
 
