c3a2c6d5afb6134642b4b4f0eb4fa55861218baf
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-13 21:43:14 +0530

    Time: 50 ms (20.33%), Space: 55.7 MB (5.23%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -7,43 +7,42 @@ class Solution {
             minHeap.add(new int[]{list.get(0), 0, index++});
         }
                 
+        int start = 0, min = Integer.MAX_VALUE;
+        int  startAns=0, endAns = nums.size()-1;
+        index = 0;
+        
+        Map<Integer, Integer> count = new HashMap<>();
+
 
         List<int[]> sorted = new ArrayList<>();
         while(!minHeap.isEmpty()) {
             int[] curr = minHeap.poll();
+            count.put(curr[2], count.getOrDefault(curr[2], 0)+1);
             sorted.add(curr);
+            while(count.size() == nums.size()) {
+                if(sorted.get(index)[0]-sorted.get(start)[0]<min) {
+                    startAns = sorted.get(start)[0];
+                    endAns = sorted.get(index)[0];
+                    min = sorted.get(index)[0]-sorted.get(start)[0];
+                }
+                int current = count.get(sorted.get(start++)[2]);
+                if(current == 1) {
+                    count.remove(sorted.get(start-1)[2]);
+                } else {
+                    count.put(sorted.get(start-1)[2], current-1);
+                }
+            }
+            index++;
+            
+            
             if(curr[1] < nums.get(curr[2]).size()-1) {
                 minHeap.add(new int[]{nums.get(curr[2]).get(curr[1]+1), curr[1]+1, curr[2]});
             }
         }
-        Map<Integer, Integer> count = new HashMap<>();
-        
-        int start = 0, min = Integer.MAX_VALUE;
-        int  startAns=0, endAns = 0;
-        index = 0;
         
         
         
-        for(int[] arr: sorted) {
-            count.put(arr[2], count.getOrDefault(arr[2], 0)+1);
-            while(count.size() == nums.size()) {
-                if(sorted.get(index)[0]-sorted.get(start)[0]<min) {
-                    startAns = start;
-                    endAns = index;
-                    min = sorted.get(index)[0]-sorted.get(start)[0];
-                }
-                int curr = count.get(sorted.get(start++)[2]);
-                if(curr == 1) {
-                    count.remove(sorted.get(start-1)[2]);
-                } else {
-                    count.put(sorted.get(start-1)[2], curr-1);
-                }
-            }
-            index++;
-
-        }
-        
-        return new int[]{sorted.get(startAns)[0], sorted.get(endAns)[0]};
+        return new int[]{startAns, endAns};
     }
     
 }
