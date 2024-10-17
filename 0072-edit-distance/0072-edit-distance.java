db6577279b0bf995d81b9d4efa2cfa6cb39df89f
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-17 10:25:23 +0530

    Time: 3 ms (97.00%), Space: 44.6 MB (87.21%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,31 +1,35 @@
 class Solution {
-    
-    Map<String, Integer> cache = new HashMap<>();
+    int[][] cache;
     public int minDistance(String word1, String word2) {
+        cache = new int[ word1.length()][word2.length()];
+
         return count(0, 0, word1, word2);
     }
     
     
     int count(int index1, int index2, String word1, String word2) {
-        String key = index1+":"+index2;
-        if(cache.containsKey(key)) {
-            return cache.get(key);
-        }
+        
         
         int count = 0;
 
         
         if(index1 == word1.length()) {
-            count = word2.length()-index2;
+            return word2.length()-index2;
         } else if(index2 == word2.length()) {
-            count = word1.length()-index1;
-        } else if(word1.charAt(index1) == word2.charAt(index2)) {
+            return word1.length()-index1;
+        }  
+        
+        if(cache[index1][index2] !=0) {
+            return cache[index1][index2];
+        }
+        
+        if(word1.charAt(index1) == word2.charAt(index2)) {
             count = count(index1+1, index2+1, word1, word2);
         } else {
             count = 1 + Math.min(count(index1, index2+1, word1, word2), Math.min(count(index1+1, index2, word1, word2), count(index1+1, index2+1, word1, word2)));
         }
         
-        cache.put(key, count);
+        cache[index1][index2] = count;
         
         return count;
         
