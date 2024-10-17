56e7afca61e59a620703857c2a643573927d3603
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-17 10:45:34 +0530

    Time: 5 ms (96.95%), Space: 43 MB (95.27%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -9,7 +9,7 @@ class Solution {
                 if(nums1[i-1] == nums2[j-1]) {
                     dp[i][j] = dp[i-1][j-1]+1;
                 } else {
-                    dp[i][j] = Math.max(Math.max(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
