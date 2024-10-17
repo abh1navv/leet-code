f71312c0f7e9f664a779d1f72f1be8708de8ba0e
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-17 10:08:00 +0530

    Time: 26 ms (5.24%), Space: 45.3 MB (8.16%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    Map<String, Integer> cache = new HashMap<>();
+    public int minDistance(String word1, String word2) {
+        return count(0, 0, word1, word2);
+    }
+    
+    
+    int count(int index1, int index2, String word1, String word2) {
+        String key = index1+":"+index2;
+        if(cache.containsKey(key)) {
+            return cache.get(key);
+        }
+        
+        if(index1 == word1.length() && index2 == word2.length()) {
+            return 0;
+        } else if(index1 == word1.length()) {
+            return word2.length()-index2;
+        } else if(index2 == word2.length()) {
+            return word1.length()-index1;
+        }
+        
+        int count = 0;
+        if(word1.charAt(index1) == word2.charAt(index2)) {
+            count = count(index1+1, index2+1, word1, word2);
+        } else {
+            count = 1 + Math.min(count(index1, index2+1, word1, word2), Math.min(count(index1+1, index2, word1, word2), count(index1+1, index2+1, word1, word2)));
+        }
+        
+        cache.put(key, count);
+        
+        return count;
+        
+    }
+}
