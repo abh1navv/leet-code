c71402b64cb11ef396ce6fbb45a6170166f4ec06
Abhinav Pandey <abhinavpandey312@gmail.com>
2024-10-19 16:11:43 +0530

    Time: 739 ms (5.14%), Space: 40.9 MB (92.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,16 @@
 class Solution {
+    
     public int findTargetSumWays(int[] nums, int target) {
         return findWays('+', 0, 0, target, nums) + findWays('-', 0, 0, target, nums);
     }
     
     int findWays(char sign, int index, int sum, int target, int[] nums) {
         
-        
         int count = 0;
         
         if(sign == '+') sum+=nums[index];
         else sum-=nums[index];
-        
-        //System.out.println(sum);
-        
+                
         if(index == nums.length-1 && sum == target) return 1;
         else if(index== nums.length-1) return 0;
         
